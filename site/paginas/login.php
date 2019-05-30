92e2ae22256312db68c7b1d7d616903935910184
LucasRocha99 <lucasrodriguesrocha99@gmail.com>
2019-05-29 21:03:03 -0300

    login

---
+++ site/paginas/login.php
@@ -0,0 +1,37 @@
+<div id="login">
+  <link rel="stylesheet" type="text/css" href=" public/css/login.css">  
+        <div class="container">
+            <div class="row">
+             <div class="col-md-5 mx-auto">
+             <div id="first">
+                <div class="myform form ">
+                    <div class="logo mb-3">
+                       <div class="col-md-12 text-center">
+                         <h1>Login</h1>
+                       </div>
+                   </div>
+                       <form action="" method="post" name="login">
+                               <div class="form-group">
+                                  <label for="exampleInputEmail1">Email</label>
+                                  <input type="email" name="email"  class="form-control" id="email" aria-describedby="emailHelp" placeholder="Ensira o seu email">
+                               </div>
+                               <div class="form-group">
+                                  <label for="exampleInputEmail1">Senha</label>
+                                  <input type="password" name="password" id="password"  class="form-control" aria-describedby="emailHelp" placeholder="Ensira a sua senha">
+                               </div>
+                               
+                               <div class="col-md-12 text-center ">
+                                  <button type="submit" class=" btn btn-block mybtn btn-primary tx-tfm">Login</button>
+                               </div>
+                               <div class="col-md-12 ">
+                                  
+                               </div>
+                               
+                            </form>
+                     
+                      </div>
+                </div>
+        </div>
+    </div>
+</div>
+         
